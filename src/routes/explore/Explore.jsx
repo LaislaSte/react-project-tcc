fc53084436a6e4605921ea1ac6ba300fc66e4fb0
LaislaSte <stephenielaisla@gmail.com>
2022-09-14 17:21:06 -0300

    modificado rota explore

--- src/routes/explore/Explore.jsx
+++ src/routes/explore/Explore.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 // import { Posts } from './ExploreData';
-// import { Post } from '../../components/post/Post';
+import { Post } from '../../components/post/Post';
 import Navbar from '../../components/navbar/Navbar';
 import Input from '../../components/input/Input';
 
@@ -11,7 +11,7 @@ import { BsSearch } from 'react-icons/bs';
 const Explore = () => {
 
     // const [resulSearch, setResultSearch] = useState([]);
-    // const [term, setTerm] = useState('');
+    const [term, setTerm] = useState('');
 
     // const onSearch = (e) => {
     //     setTerm(e.target.value);
@@ -47,11 +47,51 @@ const Explore = () => {
     // const onClickResultSearch = (id) => {
     //     console.log('ao clicar no btn resultado da pesquisa', { id });
     // }
+    const category = [
+        'matemática',
+        'história',
+        'português',
+        'química'
+    ]
 
     return (
         <>
-        <h1>Tela da Home</h1>
-        <Navbar />
+            <h1>Tela da Home</h1>
+            <Navbar />
+            <header className="header-main-filter">
+                <div className="filter-input-text">
+                    <Input
+                        text='Pesquisar'
+                        type='text'
+                        className='input-outline-secondary'
+                        value={term}
+                        onchange={(e) => {setTerm(e.target.value)}}
+                    />
+
+                </div>
+                <div className="filter-input-select">
+                    <select name="" id="">
+                        <option value="Selecione uma Categoria">Selecione uma categoria</option>
+                        {category.map((item, index) => {
+                            return (
+                                <option value={item} key={index}> {item} </option>
+                            )
+                        })}
+                    </select>
+                </div>
+            </header>
+
+            <main className="posts-container">
+                <Post
+                    content='dfdfdssssssssfs'
+                />
+                <Post
+                    content='dfdfdssssssssfs'
+                />
+                <Post
+                    content='dfdfdssssssssfs'
+                />
+            </main>
 
         </>
     )
