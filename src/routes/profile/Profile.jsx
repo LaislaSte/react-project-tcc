37ad65b66d6d8384a2d1dbff07f3d9764fcc09e4
LaislaSte <stephenielaisla@gmail.com>
2022-10-18 00:33:29 -0300

    add nome novo

---
+++ src/routes/profile/Profile.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import './Profile.css';
+import { Post } from '../../components/post/Post';
+import Explore from '../../assets/Explore.jpg';
+import InputImg from '../../components/inputImg/InputImg';
+import { useState } from 'react';
+
+const Profile = () => {
+    //pegar o id do usuario logado
+    //pegar foto de perfil deste usuario e colocar como foto de perfil
+    //pegar nome do usuario e colocar no nome de usuario
+    //pegar biografia do usuario e colocar em bios (biografia)
+    //puxar postagens realizadas pelo usuario
+    //mapear id e conteudo das postagens atreladas ao usuario logado na lista não ordenada retornando uma li com o componente post e suas props conteudo referente ao post, foto de perfil e nome do usuario logado
+
+    // const imgProfile = <img src={Explore} alt="foto de perfil do usuario" className='avatar'/>
+    const [image, setImage] = useState(null);
+    console.log(image);
+
+    const fakeUser = {
+        id: 1,
+        name: 'laisla',
+        avatar: 'IP',
+        posts: [
+            {
+                idPost: '1',
+                userPostContent: 'blabablabalbalbalbalalskkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkblabablabalbalbalbalalskkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkblabablabalbalbalbalalskkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk',
+                userPostImg: 'imagem da postagem do usuario'
+            },
+            {
+                idPost: '2',
+                userPostContent: 'blabablabalbalbalbal',
+                userPostImg: 'imagem da postagem do usuario'
+            },
+            {
+                idPost: '3',
+                userPostContent: 'blabablabalbalbalbal',
+                userPostImg: 'imagem da postagem do usuario'
+            }
+        ]
+    }
+
+    return (
+        <div className='Profile'>
+            <header className='section-profile'>
+                <div className="profile-container">
+                    <div className="img-background">
+                        {fakeUser.avatar}
+                    </div>
+                    <h2>Salve Jorge</h2>
+                </div>
+                <div className="bios">
+                    Tenho 24 anos, sou formado na Unicid em pedagogia e na USP em ciencias humanas, sou um homem gay, casado e com 5 filhos
+                </div>
+            </header>
+
+            <main className="section-posts">
+                <ul>
+                    <li> <h1>Postagens Realizadas</h1> </li>
+                    <li>
+                        <Post />
+                    </li>
+
+                </ul>
+            </main>
+
+        </div>
+    )
+}
+
+export default Profile;
+
