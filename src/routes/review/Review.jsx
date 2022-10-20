f27edca23629bf72e543b0a53a43980c210c642b
LaislaSte <stephenielaisla@gmail.com>
2022-10-19 21:19:25 -0300

    modificado rota review

--- src/routes/review/Review.jsx
+++ src/routes/review/Review.jsx
@@ -1,101 +1,61 @@
 import React from 'react';
-import { FaHeart } from 'react-icons/fa';
-import { Post } from '../../components/post/Post';
 import Navbar from '../../components/navbar/Navbar';
-import Input from '../../components/input/Input';
-import { useState } from 'react';
-
+import ReviewButton from '../../components/reviewbutton/ReviewButton';
+import { Post } from '../../components/post/Post';
+import CreateButton from '../../components/createbutton/CreateButton';
+import imageContent from '../../assets/Explore.jpg';
+import './Review.css'
 
 const Review = () => {
-    const [popUp, setPopUp] = useState(false);
-    const showPopUp = () => setPopUp(!popUp);
+    // const texto = 
 
-    const registerReview = () => {
-        console.log('user e conteudo registrado em revisoes, setado o contador e primeira data de revisao');
-        showPopUp();
-    }
-
-    const [cat, setCat] = useState([]);
-
-    const category = [
-        'matemática',
-        'história',
-        'português',
-        'química'
+    const user = [
+        {
+            name: 'laisla',
+            icon: imageContent,
+            texto: 'A Microsoft e nossos fornecedores terceirizados usam cookies para armazenar e acessar informações tais como IDs exclusivas para fornecer, manter e melhorar nossos serviços e anúncios. Se você'
+        },
+        {
+            name: 'laisla',
+            icon: imageContent,
+            imageContent,
+            texto: 'A Microsoft e nossos fornecedores terceirizados usam cookies para armazenar e acessar informações tais como IDs exclusivas para fornecer, manter e melhorar nossos serviços e anúncios. Se você concordar, o MSN e o Microsoft Bing personalizarão o conteúdo e os anúncios que você vê. Você pode selecionar Aceito para consentir com esses usos ou clicar em Gerenciar preferências para revisar suas opções e exercer seu direito de se opor ao Interesse Legítimo quando usado. Você pode alterar sua seleção em Gerenciar Preferências na parte inferior desta página.Declaração de Privacidade'
+        },
+        {
+            name: 'laisla',
+            icon: imageContent,
+            imageContent,
+            texto: 'A Microsoft e nossos fornecedores terceirizados usam cookies para armazenar e acessar informações tais como IDs exclusivas para fornecer, manter e melhorar nossos serviços e anúncios. Se você concordar, o MSN e o Microsoft Bing personalizarão o conteúdo e os anúncios que você vê. Você pode selecionar Aceito para consentir com esses usos ou clicar em Gerenciar preferências para revisar suas opções e exercer seu direito de se opor ao Interesse Legítimo quando usado. Você pode alterar sua seleção em Gerenciar Preferências na parte inferior desta página.Declaração de Privacidade'
+        }
     ]
+
     return (
         <>
+            <Navbar />
             <header className="header-main-filter">
-                <div className="filter-input-text">
-                    <Input
-                        text='Pesquisar'
-                        type='text'
-                        className='input-outline-secondary'
-                        value={term}
-                        onchange={(e) => { setTerm(e.target.value) }}
-                    />
+                <CreateButton />
 
-                </div>
-                <div className="filter-input-select">
-                    <select name="" id="">
-                        <option value="Selecione uma Categoria">Selecione uma categoria</option>
-                        {category.map((item, index) => {
-                            return (
-                                <option value={item} key={index}> {item} </option>
-                            )
-                        })}
-                    </select>
-                </div>
             </header>
 
             <main className="posts-container">
-                <Post
-                    content='dfdfdssssssssfs'
-                />
-                <Post
-                    content='dfdfdssssssssfs'
-                />
-                <Post
-                    content='dfdfdssssssssfs'
-                />
+                {user.map((item, index) => {
+                    return (
+                        <Post
+                            key={index}
+                            content={item.texto}
+                            user_name={item.name}
+                            img_content={item.imageContent}
+                            avatar={item.icon}
+                            click_type={<ReviewButton />}
+                        />
+                    )
+                })}
             </main>
 
-            <Navbar />
-            <div className="footer-container-create">
-                <div className="footer-post">
-                    <Link to='#' className='icon-container'>
-                        <FaHeart className="icon" onClick={showPopUp} />
-                    </Link>
-                </div>
+            <footer>
 
-                <div >
-                    <section className={popUp ? 'popup-menu popup-menu-active' : 'popup-menu'}>
-                        <div className="popup-container">
-                            <Link to='#' className='icon-container close-popup'>
-                                <AiOutlineClose onClick={showPopUp} />
-                            </Link>
-                            <div className="popup-content">
-                                <h1>O que deseja memorizar?</h1>
-                                <form action="" className="form-create-post">
-                                    <textarea name="" id="" cols="10" rows="6" placeholder='Adicione uma descrição...'></textarea>
+            </footer>
 
-                                    <Input
-                                        text='Adicione uma categoria. Ex: Matemática'
-                                        className='input-outline-primary'
-                                        value={cat}
-                                        onchange={(e) => {setCat(e.target.value) }}
-
-                                    />
-                                </form>
-                                <div className="btns-popup">
-                                    <Button type='button' bg_color='secondary' fun={registerReview} text='sim' />
-                                    <Button type='button' bg_color='secondary' fun={showPopUp} text='não' />
-                                </div>
-                            </div>
-                        </div>
-                    </section>
-                </div>
-            </div>
         </>
     )
 }
