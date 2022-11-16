e1d7da1b2708b331726e03cc832a04e49e83a9c1
LaislaSte <stephenielaisla@gmail.com>
2022-11-16 16:54:58 -0300

    fix/navbar: atualizado icones

--- src/utils/arraysNavbar.js
+++ src/utils/arraysNavbar.js
@@ -1,35 +1,29 @@
 import React, { useContext } from 'react';
 import avatarDefault from '../assets/icons/avatarDefault.svg';
 
-import { BiHomeAlt, BiLogOut } from 'react-icons/bi';
+import { BiHomeAlt, BiLogOut, BiLogIn, BiInfoCircle } from 'react-icons/bi';
 import { RiSearch2Line } from 'react-icons/ri';
-import { BsPersonCircle, BsFillPlusCircleFill } from 'react-icons/bs';
+import { BsPersonCircle } from 'react-icons/bs';
 import { FaGraduationCap } from 'react-icons/fa';
-import { IoMdCreate, IoLogOutOutline } from 'react-icons/io'
-// import { logout } from '../services/googleAuthenticatios';
-import { signOut } from 'firebase/auth';
-import { auth } from '../services/Banco';
-// import { CostumerContext } from '../services/UserContext';
-
-// const { logout } = useContext(CostumerContext);
+import { IoMdCreate} from 'react-icons/io';
 
 export const SidebarDataPublic = [
     {
         title: 'Login',
         path: '/login',
-        icon: <BiHomeAlt />,
+        icon: <BiLogIn />,
         cName: 'nav-text'
     },
     {
         title: 'Cadastrar',
         path: '/register',
-        icon: <RiSearch2Line />,
+        icon: <BsPersonCircle />,
         cName: 'nav-text'
     },
     {
         title: 'Sobre',
         path: '/about-us',
-        icon: <BsPersonCircle />,
+        icon: <BiInfoCircle />,
         cName: 'nav-text'
     },
 ]
